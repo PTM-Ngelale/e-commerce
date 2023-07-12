6c3a070b2049e0cd6aec37f0ff1b66619af1e7ae
Micah Ngelale <preciousngelale@gmail.com>
2023-07-12 16:52:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Shop } from "./pages/shop/Shop";
+import { Cart } from "./pages/cart/Cart";
+import { ShopContextProvider } from "./context/shop-context";
+
+export default function App() {
+  return (
+    <div className="app">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
